494796ba2246db26da2acf005538a96111f12e24
Karsten Westra <karstenwestra@gmail.com>
2022-10-26 20:57:31 +0200

    Added basic project to start and play with the SpaceX api.

---
+++ spacex/spacex/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  spacex
+//
+//  Created by Karsten Westra on 26/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.view.backgroundColor = UIColor.red
+        self.title = "Space X"
+    }
+
+
+}
+
